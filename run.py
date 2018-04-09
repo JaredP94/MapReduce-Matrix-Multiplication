3314d5535d1c2da834fa2ca51b90525f3bb74f77
Jared <jaredping@yahoo.com>
2018-04-09 11:31:19 +0200

    Sort all the things

--- run.py
+++
@@ -1,9 +0,0 @@
-from AlgorithmA import MatrixMultiplication
-
-if __name__ == '__main__':
-    mr_job = MatrixMultiplication(['--no-conf', '--runner=local', 'File1ForLab3.txt'])
-    with mr_job.make_runner() as runner:
-        tmp_output = []
-        runner.run()
-        for line in runner.stream_output():
-            tmp_output = tmp_output + [line]
